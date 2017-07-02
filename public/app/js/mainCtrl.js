74f69456f874ab5d5c7b49b1a5094161a837b4fe
loren pabst <loren pabst>
2017-07-01 23:38:02 -0600

    nav bar routes to section of page now

--- public/app/js/mainCtrl.js
+++ public/app/js/mainCtrl.js
@@ -1,8 +1,8 @@
 angular.module('app').controller('mainCtrl', function($scope){
 
     let mobileMenu = false;
-    // $scope.showHam = true;
 
+//slides mobile menu in and out and changes ham menu to an X
     $scope.toggleMobileMenu = () => {
         if (!mobileMenu){
             mobileMenu = true;
@@ -35,9 +35,23 @@ angular.module('app').controller('mainCtrl', function($scope){
         }
     }
 
+    $scope.hideMobileMenu = function(){
+        mobileMenu = false;
+        $('.mobileMenu').css({
+            'height': '0px',
+        });
+        $('#line_one').css({
+            'transform': 'rotate(0deg)'
+        })
+        $('#line_three').css({
+            'transform': 'rotate(0deg)'
+        })
+        $('#line_two').css({
+            'width': '22px'
+        })
+    }
 
-//when the page is scrolled down, the desktop header fills the top
-//of the page
+//when window scrolls, header background shows up
     function changeHeaderCss(){
         var dHeader = $('.desktop_header');
         if (this.scrollY > 50){
@@ -52,5 +66,43 @@ angular.module('app').controller('mainCtrl', function($scope){
     }
     
     window.addEventListener("scroll", changeHeaderCss, false);
+
+//Scrolls page to appropriate location based on nav bar click
+    $scope.nav = function(num){
+
+        let headerHeight;
+        if (window.innerWidth < 700){
+            headerHeight = 60;
+        }else{
+            headerHeight = 68;
+        }
+        
+        let projectsSection = document.getElementById('home_projects')
+        let skillsSection = document.getElementById('home_skills');
+        let aboutSection = document.getElementById('home_about');
+        let contactSection = document.getElementById('home_contact');
+
+        var landingPage = 0;
+        var projectsPage = window.innerHeight - headerHeight;
+        var skillsPage = projectsPage + projectsSection.scrollHeight;
+        var aboutPage = skillsPage + skillsSection.scrollHeight;
+        var contactPage = aboutPage + aboutSection.scrollHeight;
+        console.log(aboutPage)
+
+
+        if (num === 0){
+            document.body.scrollTop = 0;
+        }else if (num === 1){
+            document.body.scrollTop = projectsPage;
+        }else if (num === 2){
+            document.body.scrollTop = skillsPage;
+        }else if (num === 3){
+            document.body.scrollTop = aboutPage;
+        }else if (num === 4){
+            document.body.scrollTop = contactPage;
+        }
+
+        $scope.hideMobileMenu();
+    }
 
 });
