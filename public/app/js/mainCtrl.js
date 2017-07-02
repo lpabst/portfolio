20cffe04fe7bbbcb7756f226e70a3f643e7c494b
loren pabst <loren pabst>
2017-07-01 18:34:17 -0600

    added portfolios pieces and partial skills list

--- public/app/js/mainCtrl.js
+++ public/app/js/mainCtrl.js
@@ -35,53 +35,19 @@ angular.module('app').controller('mainCtrl', function($scope){
         }
     }
 
-    // $scope.hideMobileMenu = () => {
-    //     $scope.mobileMenu = false;
-    //     learnSubMenu = false;
-    // }
-
-
-//slides out the mobile learn submenu
-    $scope.showLearnSubMenu = () => {
-        if (!learnSubMenu){
-            $('.learn_submenu_mobile').css({
-                'left': '200px',
-                'height': '260px'
-            });
-            learnSubMenu = true;
-        }else{
-            $('.learn_submenu_mobile').css({
-                'left': '0px',
-                'height': '0px'
-            });
-            learnSubMenu = false;
-        }
-
-    }
 
 //when the page is scrolled down, the desktop header fills the top
 //of the page
     function changeHeaderCss(){
         var dHeader = $('.desktop_header');
-        var learnHeader = $('.learn_subheader');
         if (this.scrollY > 50){
             dHeader.css({
-                "width": "100%",
-                'left': '0',
-                'top': '0',
-                'border-radius': '0'
+                "background": "#333",
             });
-            $('.desktop_nav').css('right', '7.2%');
-            learnHeader.css('top', '68px');
         }else{
             dHeader.css({
-                "width": "94%",
-                'left': '3%',
-                'top': '20px',
-                'border-radius': '6px'
+                "background": "none",
             });
-            $('.desktop_nav').css('right', '50px');
-            learnHeader.css('top', 'calc(3% + 68px)');
         }
     }
     
