cfc1be3214e03b29247439bdad9a87a1ecbe2819
loren pabst <loren pabst>
2017-07-03 17:04:43 -0600

    contact portion emails me now

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -1,7 +1,24 @@
 angular.module("app")
 .controller("homeCtrl", function($scope) {
-
   
+    $scope.submitContactEmail = function(){
+        let body = $scope.contactContent;
+        if ($scope.contactName){
+            body = 'Hi, my name is ' + $scope.contactName + '. ' + body;
+        }
+        if ($scope.contactEmailOrPhone){
+            body = body + '. You can contact me back at ' + $scope.contactEmailOrPhone;
+        }
+        
+        let emailStr = 'mailto:lorenpabst@gmail.com?'
+        + `subject=${$scope.contactSubject}`
+        + '&body=' + body
 
+        window.location.href = emailStr;
+    }
+
+    $scope.linkDevMountain = function(){
+        window.open('http://www.devmountain.com');
+    }
 
 });
